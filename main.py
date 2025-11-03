a90d203d02a307df8f334bfbbf2715e81d475a45
UlisesPariente <ulisespariente1@gmail.com>
2025-11-03 11:51:39 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import funciones
+
+while True:
+    print  (f"═════════════════════════\n\tMINI GENERALA\n═════════════════════════\n[1] Jugar\n[2] Estadisticas\n[3] Creditos\n[4] Salir\n× Seleccione una opcion:\n ")
+    opcion = int(input(""))
+    
