26edbe0f0bf49730cd9a93fd697d3c05eda535d6
GonzaloRuizDiaz <ruizdiaz1020@gmail.com>
2025-11-05 08:49:36 -0300

    actualizando funcion de tiradad de dados

--- funciones.py
+++ funciones.py
@@ -23,29 +23,44 @@ Nombre_de_dados = ["A","B","C","D","E"]
 
 def tirada_de_dados ():
     dados = []
-    dado_permanentes =[0]*5
-        
-    for i in range (3): #turnos de tirada
-        flag_fin =True
-        for i in range (5): #tira dados
-            numero_del_dado = random.randint (1,6)
-            dados.append (numero_del_dado) 
-            print (f"[{dados[i]} -> {Nombre_de_dados[i]}]")
+    
+   
+    for g in range (3): 
+        print(f"------ tirada numero {g + 1} ------")
+        if g == 0:
+            for l in range (5): 
+                numero_del_dado = random.randint (1,6)
+                dados.append ([numero_del_dado,False]) 
+                print (f"[{[l + 1]}.{dados[l][0]} -> {Nombre_de_dados[l]}]")
+        else:
+            for j in range (5):
+                if dados[j][1] == False:
+                    numero_del_dado = random.randint (1,6)
+                    dados[j][0] = numero_del_dado
+                print (f"[{[j + 1]}.{dados[j][0]} -> {Nombre_de_dados[j]}]")
+
+
+        if g < 2 :   
+            for k in range(5): 
+                while True:
+                    dado_que_desea_mantener = (input("Indique la posicion del dado desea mantener(de querer salir utilizar '10'): "))
+
+                    if not dado_que_desea_mantener.isdigit():
+                        print("debe de poner solo numeros, intente de nuevo")
+                    else:
+                        break
             
-        for i in range(5): #dados a mantener
-            
-            dado_que_desea_mantener = str(input("Indique por el nombre cual dado desea mantener(de querer salir utilizar '*'): "))
-            
-            if dado_que_desea_mantener == "*":
-                flag_fin = False    
-                break
-            
-            for i in range (len(Nombre_de_dados)):
-                if dado_que_desea_mantener.lower() == Nombre_de_dados[i].lower():
-                    dado_permanentes [i] = dados[i]
+                dado_que_desea_mantener = int(dado_que_desea_mantener)
+
+                if dado_que_desea_mantener == 10:
                     
-        if flag_fin == False:
-            break
-        
-    print(dado_permanentes)
-print (tirada_de_dados())
+                    break
+            
+                for i in range (len(dados)):
+                    if dado_que_desea_mantener - 1 == i:
+                        dados[i][1] = True
+       
+       
+                    
+                    
+print (tirada_de_dados()) 
