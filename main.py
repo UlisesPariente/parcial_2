78bf9192cd7a1a0c62dfa702343793734bb881e5
UlisesPariente <ulisespariente1@gmail.com>
2025-11-05 13:41:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ while True:
     print  (f"═════════════════════════\n\tMINI GENERALA\n═════════════════════════\n[1] Jugar\n[2] Estadisticas\n[3] Creditos\n[4] Salir\n× Seleccione una opcion:\n ")
     opcion = int(input(""))
     
+    match opcion:
+        case 1:
+           pass
+                
+        case 2:
+            pass
+        
+        case 3:
+            pass
+            # creditos 
+            # "2/11/2025  ->  -/-/-"
+            
+        case 4:
+            break
+        
+        case _:
+            print (f"Opcion Invalida....")
