b7e3c65db7ed6a14416eab52751a0e9aa0fd647e
UlisesPariente <ulisespariente1@gmail.com>
2025-11-09 23:38:02 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -10,162 +10,179 @@ Puntos_GENERALA = [["[1] ","[UNO]",0,True],
                    ["[9] ","[POKER (40)]",0,True],
                    ["[10] ","[GENERALA (50)]",0,True]]
 
-def modificacion_generala (opcion_seleccionada, acumulador, puntos_generala):
-    pass
+def modificacion_generala (puntaje:int, opcion):
+    numero_opcion = int (opcion)
+    Puntos_GENERALA [numero_opcion-1][2] = puntaje
 
 def seleccion_puntaje (dados):
-    opciones_de_seleccion = input(f"Asigne el puntaje: ")
-    acumulador = 0
-    
+
     while True:
+        opciones_de_seleccion = input(f"Asigne el puntaje: ")
+        Puntaje = 0
             
         match opciones_de_seleccion:
-            case 1:
-                
-                for i in range (5):
-                    if 1 in dados[i]:
-                        acumulador += dados[i]
-                
-                
-                
-            case 2:
-                for i in range (5):
-                    if 2 in dados[i]:
-                        acumulador += dados[i]
-                
-            
-            case 3:
-                for i in range (5):
-                    if 3 in dados[i]:
-                        acumulador += dados[i]
-    
-            
-            case 4:
-                for i in range (5):
-                    if 4 in dados[i]:
-                        acumulador += dados[i]
-                
-            
-            case 5:
-                for i in range (5):
-                    if 5 in dados[i]:
-                        acumulador += dados[i]
-                
-            
-            case 6:
-                for i in range (5):
-                    if 6 in dados[i]:
-                        acumulador += dados[i]
-                
-            
-            case 7:# [5,3,2,1,4]
-                Lista_Escalera_correcta =[[1,False], [2,False],[3,False],[4,False],[5,False]]
-                flag_escalera_incorrecta = False
-                
-                for i in range (5):
-                    for j in range (5):
-                        if Lista_Escalera_correcta [i][0] == dados[j]:
-                            Lista_Escalera_correcta [i][1] = True
-                            break
-                        
-                for i in range (5):
-                    if Lista_Escalera_correcta [i][1] == False:
-                        flag_escalera_incorrecta = True
-                        
-                if flag_escalera_incorrecta == False :
-                    acumulador = 20
+            case "1":
+                if Puntos_GENERALA[0][3] == True:
+                    Puntos_GENERALA [0][3] = False 
+                    for i in range (5):
+                        if 1 in dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    print ("opcion invalida")
                     
+                # 2,5,4,4,4
                 
-            case 8:
-                #opcion = [3,3,3,2,2]
-                #dados = [2,3,2,3,3]
-                contador_1=0
-                contador_2=0
-                contador_3=0
-                contador_4=0
-                contador_5=0
-                contador_6=0
-                flag_full = True
-                for i in range (5):
-                    match dados[i]:
-                        case 1:
-                            contador_1 += 1
-                            pass
-                        case 2:
-                            contador_2 += 1
-                            pass
-                        case 3:
-                            contador_3 += 1
-                            pass
-                        case 4:
-                            contador_4 += 1
-                            pass
-                        case 5:
-                            contador_5 += 1
-                            pass
-                        case 6:
-                            contador_6 += 1
-                            pass
+            case "2":
+                if Puntos_GENERALA[1][3] == True:
+                    Puntos_GENERALA [1][3] = False 
+                    for i in range (5):
+                        if 2 in dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    print ("opcion invalida")
+                    
+            case "3":
+                if Puntos_GENERALA[2][3] == True:
+                    Puntos_GENERALA [2][3] = False 
+                    for i in range (5):
+                        if 3 in dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    print ("opcion invalida")
+                    
+            case "4":
+                if Puntos_GENERALA[3][3] == True:
+                    Puntos_GENERALA [3][3] = False 
+                    for i in range (5):
+                        if 4 in dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    print ("opcion invalida")
+                    
+            case "5":
+                if Puntos_GENERALA[4][3] == True:
+                    Puntos_GENERALA [4][3] = False 
+                    for i in range (5):
+                        if 5 in dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    print ("opcion invalida")
+                   
+            case "6":
+                if Puntos_GENERALA[5][3] == True:
+                    Puntos_GENERALA [5][3] = False 
+                    for i in range (5):
+                        if 6 in dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    print ("opcion invalida")
+                    
+            case "7":
+                if Puntos_GENERALA[6][3] == True:
+                    Puntos_GENERALA[6][3] = False
+                    Lista_Escalera_correcta_1 =[1,2,3,4,5]
+                    Lista_Escalera_correcta_2 = [2,3,4,5,6]
                 
-                            
-            case 9:
-                lista_contador =[0,0,0,0,0,0]
+                    Dados_auxiliar = dados
+                    Dados_auxiliar.sort()
+                    if Dados_auxiliar == Lista_Escalera_correcta_1 or Dados_auxiliar == Lista_Escalera_correcta_2:
+                        Puntaje = 20
+                        break
+                else:
+                    print("Opcion invalida")
+            case "8":
+                if Puntos_GENERALA[7][3] == True:
+                    Puntos_GENERALA[7][3] = False
+                    contador_full = [0,0,0,0,0,0]
+                    for i in range (5):
+                        match dados[i]:
+                            case 1:
+                                contador_full [0] += 1
+                            case 2:
+                                contador_full[1] += 1
+                            case 3:
+                                contador_full[2] += 1
+                            case 4:
+                                contador_full[3] += 1  
+                            case 5:
+                                contador_full[4] += 1
+                            case 6:
+                                contador_full[5] += 1
+                                        
+                    if 3 in contador_full and 2 in contador_full:
+                        Puntaje = 30
+                        break
+                else:
+                    print ("Opcion invalida")
+            case "9":
+                if Puntos_GENERALA[8][3] == True:
+                    Puntos_GENERALA[8][3] = False
+                    lista_contador =[0,0,0,0,0,0]
+                    
+                    for i in range (5):
+                        match dados[i]:
+                            case 1:
+                                lista_contador[0] += 1
+                            case 2:
+                                lista_contador[1] += 1
+                                
+                            case 3:
+                                lista_contador[2] += 1
+                                
+                            case 4:
+                                lista_contador[3] += 1
+                                
+                            case 5:
+                                lista_contador[4] += 1
+                                
+                            case 6:
+                                lista_contador[5] += 1
+                                
+                    if 4 in lista_contador:
+                        Puntaje = 40
+                        break
+                else:
+                    print ("Opcion invaldia")
                 
-                for i in range (5):
-                    match dados[i]:
-                        case 1:
-                            lista_contador[0] += 1
-                        case 2:
-                            lista_contador[1] += 1
-                            
-                        case 3:
-                            lista_contador[2] += 1
-                            
-                        case 4:
-                            lista_contador[3] += 1
-                            
-                        case 5:
-                            lista_contador[4] += 1
-                            
-                        case 6:
-                            lista_contador[5] += 1
-                            
-                for i in range (5):
-                    if lista_contador [i] == 4:
-                        acumulador= 40
-                        return acumulador                
-            
-            case 10:
-                lista_contador =[0,0,0,0,0,0]
-                
-                for i in range (5):
-                    match dados[i]:
-                        case 1:
-                            lista_contador[0] += 1
-                        case 2:
-                            lista_contador[1] += 1
-                            
-                        case 3:
-                            lista_contador[2] += 1
-                            
-                        case 4:
-                            lista_contador[3] += 1
-                            
-                        case 5:
-                            lista_contador[4] += 1
-                            
-                        case 6:
-                            lista_contador[5] += 1
-                            
-                for i in range (5):
-                    if lista_contador [i] == 5:
-                        acumulador= 50
-                        return acumulador
-                           
+            case "10":
+                if Puntos_GENERALA[9][3] == True:
+                    Puntos_GENERALA[9][3] = False
+                    lista_contador =[0,0,0,0,0,0]
+                    
+                    for i in range (5):
+                        match dados[i]:
+                            case 1:
+                                lista_contador[0] += 1
+                            case 2:
+                                lista_contador[1] += 1
+                                
+                            case 3:
+                                lista_contador[2] += 1
+                                
+                            case 4:
+                                lista_contador[3] += 1
+                                
+                            case 5:
+                                lista_contador[4] += 1
+                                
+                            case 6:
+                                lista_contador[5] += 1
+                                
+                    if 5 in lista_contador:
+                        Puntaje = 50
+                        break
+                else:
+                    print ("Opcion invalida")        
             case _:
                 print(f"Opcion invalida...")
 
-        return acumulador, opciones_de_seleccion
+        return Puntaje, opciones_de_seleccion
         
 def puntuaciones ():
     print("═════════════════════════\n\tPLANTILLA DE PUNTAJES\n═════════════════════════\n")
@@ -175,7 +192,6 @@ def puntuaciones ():
         Puntos_totales += Puntos_GENERALA[i][1]
     print(f"═════════════════════════\nPUNTAJE TOTAL: {Puntos_totales}\n═════════════════════════\n")
     return Puntos_totales
-puntuaciones()
 
 Nombre_de_dados = ["A","B","C","D","E"]
 
