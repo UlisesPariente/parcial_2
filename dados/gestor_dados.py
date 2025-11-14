8fd15959e9e5917c69724e7e431ac4bcf5e3bad1
UlisesPariente <ulisespariente1@gmail.com>
2025-11-14 20:27:15 -0300

    Update gestor_dados.py

--- dados/gestor_dados.py
+++ dados/gestor_dados.py
@@ -1,21 +1,27 @@
 from dados.lanzada_dados import tirada_de_dados
 
 def dados_manetener (dados):
-    
     for i in range (2):
-        opcion = str(input("Desea hacer otra tirada? s/n\n")).lower()
+        while True:
+            opcion = str(input(f"TIRADAS DISPOBINLES {i+1} de 3\n»Desea tirar otra vez? s/n\n»")).lower()
+            if opcion == "s" or opcion == "n":
+                break
+            else:
+                print ("Opcion invalida, reintentar....")
+                
         if opcion == "s":
             for k in range(5):
                  
                 while True:
-                    dado_que_desea_mantener = (input("Indique la posicion del dado desea mantener(de querer salir utilizar'10'): "))
+                    dado_que_desea_mantener = (input(f"»Indique la posicion del dado desea mantener (de querer salir presione '0'):\n "))
                     if not dado_que_desea_mantener.isdigit():
                         print("debe de poner solo numeros, intente de nuevo")
                     else:
-                        break    
-                
-                if dado_que_desea_mantener == "10":
+                        break
+                       
+                if dado_que_desea_mantener == "0":
                     break
+                
                 dado_que_desea_mantener = int(dado_que_desea_mantener)
                                     
                 for i in range (len(dados)):
@@ -42,7 +48,3 @@ def lista_de_dados_elegidos (lista_dados):
     return dados_elegidos
 
 
-def nombre_dados (datos, dados):
-    for i in range(6):
-        if datos["nombres"][i][1] == dados:
-            return datos["nombres"][i][0]
