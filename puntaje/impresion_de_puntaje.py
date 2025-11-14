0e39f6cfa36b8d3917a71c38193d1efbf17e4336
UlisesPariente <ulisespariente1@gmail.com>
2025-11-14 20:28:01 -0300

    Update impresion_de_puntaje.py

--- puntaje/impresion_de_puntaje.py
+++ puntaje/impresion_de_puntaje.py
@@ -1,16 +1,24 @@
 
 from datos.datos import DATOS
+
 def mostrar_puntuaciones ():
     print("════════════════════════════════════")
     print("\tPLANTILLA DE PUNTAJES")
     print("════════════════════════════════════\n")
 
     Puntos_totales = 0
+    ancho = max(len(item['jugada']) for item in DATOS["puntos"])
 
-    for i, item in enumerate(DATOS ["puntos"], start=1):
-        print(f"[{i}] \t{item['jugada']}\t:\t{item['valor']}")
+    for i, item in enumerate(DATOS["puntos"], start=1):
+       
+        print(f"[{i:2}] {item['jugada']:<{ancho}}  :  {item['valor']:>3}")
         Puntos_totales += item["valor"]
-
     print("\n════════════════════════════════════")
     print(f"PUNTAJE TOTAL:\t{Puntos_totales}")
     print("════════════════════════════════════\n")
+
+
+def leer_TOP10_linea_por_linea(ruta):
+    with open(ruta, "r", encoding="utf-8") as archivo:
+        for linea in archivo:
+            print(linea.strip())
