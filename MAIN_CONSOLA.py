b11c708a0a2e4152e796c057296f04eb4370d7a0
UlisesPariente <ulisespariente1@gmail.com>
2025-11-14 20:27:38 -0300

    Create MAIN_CONSOLA.py

---
+++ MAIN_CONSOLA.py
@@ -0,0 +1,52 @@
+from dados.gestor_dados import *
+from puntaje.guardado_de_puntaje import *
+from puntaje.asignacion_puntaje import *
+from puntaje.impresion_de_puntaje import *
+from datos.datos import DATOS
+
+while True:
+    print  (f"═════════════════════════\n\tMINI GENERALA\n═════════════════════════\n[1] Jugar\n[2] Estadisticas\n[3] Creditos\n[4] Salir\n× Seleccione una opcion:\n ")
+    opcion = int(input(""))
+    
+    match opcion:
+        case 1:
+            for i in range (len(DATOS ["puntos"])):
+                DATOS ["puntos"][i]["valor"]=0
+                DATOS ["puntos"][i]["bandera_uso"]=True
+                
+        
+            for i in range(10):
+                dados = [[0,False],[0,False],[0,False],[0,False],[0,False]]
+            
+                mostrar_puntuaciones()
+                dados = tirada_de_dados(dados)
+                dados_mantener = dados_manetener(dados)
+                dados_elegidos = lista_de_dados_elegidos(dados_mantener)
+                
+                (puntaje,opcion) = seleccion_puntaje(dados_elegidos)
+                modificacion_generala(puntaje, opcion)
+
+            puntuacion = suma_puntos()
+            nombre = input("ingrese su nombre: ")
+            guardar_puntuacion(nombre, puntuacion)
+            
+                
+        case 2:
+            
+            leer_TOP10_linea_por_linea("puntuaciones.csv")
+            pass
+        
+        case 3:
+            pass
+            # creditos 
+            # "2/11/2025  ->  -/-/-"
+            
+        case 4:
+            #QUIT
+            break
+        
+        case _:
+            print (f"Opcion Invalida....")
+
+
+        
