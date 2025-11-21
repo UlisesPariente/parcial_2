214d387ed3b47480301970d310ae921f0faf53aa
UlisesPariente <ulisespariente1@gmail.com>
2025-11-21 17:30:56 -0300

    Update evento.py

--- segundo_intento/gestion_eventos/evento.py
+++ segundo_intento/gestion_eventos/evento.py
@@ -1,32 +1,38 @@
 import pygame
+from audio.gestor_audio import cargar_efecto,EFECTO_CLICK,reproducir_efecto
+
 
 def gestionar_eventos (evento,pantalla_actual,botones):
     if botones is None:
         return pantalla_actual
     if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
         pos  = pygame.mouse.get_pos ()
-        print ( pos)
         
         for boton in botones:
             if boton["rect"].collidepoint(pos):
+                
                 print ("Click detected: sobre",boton["accion"])
+                
+                reproducir_efecto(cargar_efecto(EFECTO_CLICK))
                 
                 accion =boton["accion"]
                 
+                    
                 if pantalla_actual == "menu":
                     
-                    
-                    if accion == "jugar":
-                        return "jugar"
-                    if accion == "opciones":
-                        return "opciones"
-                    if accion == "estadisticas":
-                        return "estadisticas"
-                    if accion == "creditos":
-                        return "creditos"
-                    if accion == "salir":
-                        return "salir"
+                    match accion:
+                        case "jugar":
+                            return "jugar"
+                        case "opciones":
+                            return "opciones"
+                        case "estadisticas":
+                            return "estadisticas"
+                        case "creditos":
+                            return "creditos"
+                        case "salir":
+                            return "salir"
     return pantalla_actual
+                    
                     
                     
                     
