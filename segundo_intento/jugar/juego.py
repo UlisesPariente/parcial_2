4cf9fadf716fe832325117d8eb936304481bf31d
UlisesPariente <ulisespariente1@gmail.com>
2025-11-26 00:52:57 -0300

    Update juego.py

--- segundo_intento/jugar/juego.py
+++ segundo_intento/jugar/juego.py
@@ -1,4 +1,25 @@
 import pygame
+import random
 from segundo_intento.datos.datos import DATOS
 from dados.lanzada_dados import nombre_dados, tirada_de_dados
 
+def lista_de_dados_elegidos (lista_dados):
+    dados_elegidos = []
+  
+    for i in range  (5):
+        dados_elegidos.append(lista_dados[i][0])
+    
+    return dados_elegidos
+
+def nombre_dados (datos, dados):
+    for i in range(6):
+        if datos["nombres"][i][1] == dados:
+            return datos["nombres"][i][0]
+
+def tirada_de_dados (dados):
+        for j in range (5):
+            if dados[j][1] == False:
+                numero_del_dado = random.randint (1,6)
+                dados[j][0] = numero_del_dado
+
+        return (dados)
