e76331ea7722535f8a8da3775ff7046900017277
UlisesPariente <ulisespariente1@gmail.com>
2025-11-28 05:34:25 -0300

    Update juego.py

--- segundo_intento/jugar/juego.py
+++ segundo_intento/jugar/juego.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 from datos.datos import DATOS
-
+import os
 
 def lista_de_dados_elegidos (lista_dados):
     dados_elegidos = []
@@ -23,3 +23,229 @@ def tirada_de_dados (dados):
                 dados[j][0] = numero_del_dado
 
         return (dados)
+    
+    
+
+def seleccion_puntaje (opciones_de_seleccion,dados,font,pantalla):
+
+    while True:
+        mostrar_mensaje = True
+        tiempo_inicio = pygame.time.get_ticks()
+        if not mostrar_mensaje:
+            texto = font.render ("OPCION INVALIDA..",True,(255,255,0))
+            pantalla.blit(texto,(300,400))
+        Puntaje = 0
+            
+        match opciones_de_seleccion:
+            case 0:
+                if DATOS ["puntos"][0]["bandera_uso"] == True:
+                    DATOS ["puntos"] [0]["bandera_uso"] = False 
+                    for i in range (5):
+                        if 1 == dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+                
+                
+            case 1:
+                if DATOS ["puntos"][1]["bandera_uso"] == True:
+                    DATOS ["puntos"] [1]["bandera_uso"] = False 
+                    for i in range (5):
+                        if 2 == dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+            case 2:
+                if DATOS ["puntos"][2]["bandera_uso"] == True:
+                    DATOS ["puntos"] [2]["bandera_uso"] = False 
+                    for i in range (5):
+                        if 3 == dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+            case 3:
+                if DATOS ["puntos"][3]["bandera_uso"] == True:
+                    DATOS ["puntos"] [3]["bandera_uso"] = False 
+                    for i in range (5):
+                        if 4 == dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+            case 4:
+                if DATOS ["puntos"][4]["bandera_uso"] == True:
+                    DATOS ["puntos"] [4]["bandera_uso"] = False 
+                    for i in range (5):
+                        if 5 == dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    mostrar_mensaje = False
+                   
+            case 5:
+                if DATOS ["puntos"][5]["bandera_uso"] == True:
+                    DATOS ["puntos"] [5]["bandera_uso"] = False 
+                    for i in range (5):
+                        if 6 == dados[i]:
+                            Puntaje += dados[i]
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+            case 6:
+                if DATOS ["puntos"][6]["bandera_uso"] == True:
+                    DATOS ["puntos"][6]["bandera_uso"] = False
+                    Lista_Escalera_correcta_1 =[1,2,3,4,5]
+                    Lista_Escalera_correcta_2 = [2,3,4,5,6]
+                
+                    Dados_auxiliar = dados
+                    Dados_auxiliar.sort()
+                    
+                    if Dados_auxiliar == Lista_Escalera_correcta_1 or Dados_auxiliar == Lista_Escalera_correcta_2:
+                        Puntaje = 20
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+            case 7:
+                if DATOS ["puntos"][7]["bandera_uso"] == True:
+                    DATOS ["puntos"][7]["bandera_uso"] = False
+                    contador_full = [0,0,0,0,0,0]
+                    for i in range (5):
+                        match dados[i]:
+                            case 1:
+                                contador_full [0] += 1
+                            case 2:
+                                contador_full[1] += 1
+                            case 3:
+                                contador_full[2] += 1
+                            case 4:
+                                contador_full[3] += 1  
+                            case 5:
+                                contador_full[4] += 1
+                            case 6:
+                                contador_full[5] += 1
+                                        
+                    if 3 in contador_full and 2 in contador_full:
+                        Puntaje = 30
+                    break
+                else:
+                    mostrar_mensaje = False
+                    
+            case 8:
+                if DATOS ["puntos"][8]["bandera_uso"] == True:
+                    DATOS ["puntos"][8]["bandera_uso"] = False
+                    lista_contador =[0,0,0,0,0,0]
+                    
+                    for i in range (5):
+                        match dados[i]:
+                            case 1:
+                                lista_contador[0] += 1
+                            case 2:
+                                lista_contador[1] += 1
+                                
+                            case 3:
+                                lista_contador[2] += 1
+                                
+                            case 4:
+                                lista_contador[3] += 1
+                                
+                            case 5:
+                                lista_contador[4] += 1
+                                
+                            case 6:
+                                lista_contador[5] += 1
+                                
+                    if 4 in lista_contador:
+                        Puntaje = 40
+                    break
+                else:
+                    mostrar_mensaje = False
+                
+            case 9:
+                if DATOS ["puntos"][9]["bandera_uso"] == True:
+                    DATOS ["puntos"][9]["bandera_uso"] = False
+                    lista_contador =[0,0,0,0,0,0]
+                    
+                    for i in range (5):
+                        match dados[i]:
+                            case 1:
+                                lista_contador[0] += 1
+                            case 2:
+                                lista_contador[1] += 1
+                                
+                            case 3:
+                                lista_contador[2] += 1
+                                
+                            case 4:
+                                lista_contador[3] += 1
+                                
+                            case 5:
+                                lista_contador[4] += 1
+                                
+                            case 6:
+                                lista_contador[5] += 1
+                                
+                    if 5 in lista_contador:
+                        Puntaje = 50
+                    break
+                else:
+                    mostrar_mensaje = False
+                            
+            case _:
+                mostrar_mensaje = False
+                
+        pygame.display.flip()
+
+    return (Puntaje, opciones_de_seleccion)
+        
+
+def modificacion_generala (puntaje:int, opcion):
+    numero_opcion = int (opcion)
+    DATOS ["puntos"] [numero_opcion-1]["valor"] = puntaje
+    
+def suma_puntos ():
+    puntaje_total = 0
+    for i in range(len(DATOS["puntos"])):
+        puntaje_total +=  DATOS ["puntos"][i]["valor"]
+    return puntaje_total
+
+def guardar_puntuacion(nombre, puntuacion):
+    archivo = "puntuaciones.csv"
+    puntuaciones = []
+    
+    if os.path.exists(archivo):
+        with open(archivo, "r", encoding="utf-8") as f:
+            lineas = f.readlines()
+
+        for linea in lineas:
+            partes = linea.strip().split(",")
+            if len(partes) == 2 and partes[1].isdigit():
+                n = partes[0]
+                p = int(partes[1])
+                puntuaciones.append((n, p))
+
+    
+    puntuaciones.append((nombre, puntuacion))
+
+    n = len(puntuaciones)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if puntuaciones[j][1] < puntuaciones[j + 1][1]:
+                temp = puntuaciones[j]
+                puntuaciones[j] = puntuaciones[j + 1]
+                puntuaciones[j + 1] = temp
+
+    if len(puntuaciones) > 10:
+        puntuaciones = puntuaciones[:10]
+
+    with open(archivo, "w", encoding="utf-8") as f:
+        for n, p in puntuaciones:
+            f.write(f"{n},{p}\n")
