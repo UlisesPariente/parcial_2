aaafb087b7d39ed7ff077aaa624e5a6010e300b7
UlisesPariente <ulisespariente1@gmail.com>
2025-11-30 05:32:36 -0300

    Update lanzada_dados.py

--- dados/lanzada_dados.py
+++ dados/lanzada_dados.py
@@ -1,5 +1,5 @@
 import random
-from segundo_intento.datos.datos import DATOS
+from datos.datos import DATOS
 
 def nombre_dados (datos, dados):
     for i in range(6):
@@ -7,9 +7,10 @@ def nombre_dados (datos, dados):
             return datos["nombres"][i][0]
 
 def tirada_de_dados (dados):
-        for j in range (5):
-            if dados[j][1] == False:
-                numero_del_dado = random.randint (1,6)
-                dados[j][0] = numero_del_dado
-
-        return (dados)
+    print ("DADOS:")
+    for j in range (5):
+        if dados[j][1] == False:
+            numero_del_dado = random.randint (1,6)
+            dados[j][0] = numero_del_dado
+            print (f"[{j+1}] {dados[j][0]}")
+    return (dados)
