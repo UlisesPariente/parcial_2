f338bf967e05e3d69388c4c80b8d8a82fcf67dae
UlisesPariente <ulisespariente1@gmail.com>
2025-11-30 05:32:42 -0300

    Create datos.py

---
+++ datos/datos.py
@@ -0,0 +1,23 @@
+import json
+DATOS = {"nombres": [["MERCURIO",1],["VENUS",2],["TIERRA",3],[ "MARTE",4],["JUPITER",5],["SATURNO",6]],
+         "puntos" : [{"jugada":"[UNO]", "valor":0, "bandera_uso":True},
+                   {"jugada":"[DOS]","valor":0,"bandera_uso":True},
+                   {"jugada":"[TRES]","valor":0,"bandera_uso":True},
+                   {"jugada":"[CUATRO]","valor":0,"bandera_uso":True},
+                   {"jugada":"[CINCO]","valor":0,"bandera_uso":True},
+                   {"jugada":"[SEIS]","valor":0,"bandera_uso":True},
+                   {"jugada":"[ESCALERA (20)]","valor":0,"bandera_uso":True},
+                   {"jugada":"[FULL (30)]","valor":0,"bandera_uso":True},
+                   {"jugada":"[POKER (40)]","valor":0,"bandera_uso":True},
+                   {"jugada":"[GENERALA (50)]","valor":0,"bandera_uso":True}]}
+
+informacion = [
+        "Autores: Gonzalo Ruiz Diaz y Ulises Pariente",
+        "Fecha: 3/11/2025 - 30/11/2025",
+        "Materia: Programación 1",
+        "Docentes: Martin Alejandro Garcia",
+        "Carrera: Tecnicatura en programacion",
+        "Email de contacto: ruizdiaz1020@gmail.com/ulisespariente1@gmail.com",
+    ]
+
+
